4014f912d0f382b05c6640e57c92bd02aa7f506c
8lat <90806701+8lat@users.noreply.github.com>
2026-04-04 21:54:22 -0700

    1

--- js/maintenanceoff.js
+++ js/maintenanceoff.js
@@ -1,6 +1,6 @@
-/*
+
 
 if (window.location.pathname !== "/maintenance/" && window.location.pathname !== "/maintenance/index.html") {
     window.location.replace("/maintenance/"); 
 }
-    */
+    
